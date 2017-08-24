eb1376b3fca58f01b4f156ede716fd9d19bd0e02
jsohn96 <jsohn96@hotmail.com>
2017-08-24 01:46:37 -0700

    Put back where object was

--- Assets/PickupHandler.cs
+++ Assets/PickupHandler.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class PickupHandler : MonoBehaviour {
 	Camera _mainCamera;
 	bool _carrying;
+	bool _dropping = false;
 	GameObject _carriedObject;
+	GameObject _droppingObject;
 	[SerializeField] float _distance;
 	[SerializeField] float _smooth;
 
 	Timer _pickUpTimer;
-	[SerializeField] float _pickUpDuration = 2.0f;
+	[SerializeField] float _pickUpDuration = 0.5f;
+	Timer _dropOffTimer;
+	[SerializeField] float _dropOffDuration = 0.5f;
 
 	Vector3 _forwardRotation = new Vector3 (90.0f, 0.0f, 0.0f);
 	Quaternion _tempOriginRotation;
 	Vector3 _tempOriginPosition;
+
+	Quaternion _tempDropOriginRotation;
+	Vector3 _tempDropOriginPosition;
+	Quaternion _tempDropGoalRotation;
+	Vector3 _tempDropGoalPosition;
+
+	Collider _droppingCollider;
 
 	// Set Layer Mask to Traversal
 	int _traversalExclusionLayerMask = 1 << 8;
@@ -22,7 +33,7 @@ public class PickupHandler : MonoBehaviour {
 	void Awake () {
 			_mainCamera = Camera.main;
 			_pickUpTimer = new Timer (_pickUpDuration);
-
+			_dropOffTimer = new Timer (_dropOffDuration);
 			//include all but Traversal Layer
 		_traversalExclusionLayerMask = ~_traversalExclusionLayerMask;
 	}
@@ -33,6 +44,15 @@ public class PickupHandler : MonoBehaviour {
 			Carry(_carriedObject);
 			CheckDrop();
 		} else {
+			if(_dropping){
+				if(!_dropOffTimer.IsOffCooldown){
+					Vector3 tempPos = Vector3.Lerp(_tempDropOriginPosition, _tempDropGoalPosition, _dropOffTimer.PercentTimePassed);
+					Quaternion tempRot = Quaternion.Lerp(_tempDropOriginRotation, _tempDropGoalRotation, _dropOffTimer.PercentTimePassed);
+					_droppingObject.transform.SetPositionAndRotation(tempPos, tempRot);
+				} else {
+					FinishDrop();
+				}
+			}
 			Pickup();
 		}
 	}
@@ -62,11 +82,13 @@ public class PickupHandler : MonoBehaviour {
 			if(Physics.Raycast(ray, out hit, Mathf.Infinity, _traversalExclusionLayerMask)){
 				Pickupable p = hit.collider.GetComponent<Pickupable>();
 				if(p != null){
+					FinishDrop();
 					_pickUpTimer.Reset();
 					_carrying = true;
 					_carriedObject = p.gameObject;
 					_tempOriginPosition = _carriedObject.transform.localPosition;
 					_tempOriginRotation = _carriedObject.transform.rotation;
+					hit.collider.isTrigger = true;
 					p.GetComponent<Rigidbody>().isKinematic = true;
 				}
 			}
@@ -79,9 +101,30 @@ public class PickupHandler : MonoBehaviour {
 		}
 	}
 
+	void FinishDrop(){
+		if(_dropping){
+			_droppingObject.transform.SetPositionAndRotation(_tempDropGoalPosition, _tempDropGoalRotation);
+			_droppingCollider.isTrigger = false;
+			_droppingCollider.enabled = true;
+			_droppingObject.GetComponent<Rigidbody>().isKinematic = false;
+			_dropping = false;
+		}
+	}
+
 	void DropObject(){
+		_tempDropGoalRotation = _tempOriginRotation;
+		_tempDropGoalPosition = _tempOriginPosition;
+
+		_droppingObject = _carriedObject;
+		_tempDropOriginPosition = _droppingObject.transform.localPosition;
+		_tempDropOriginRotation = _droppingObject.transform.rotation;
+
+		_droppingCollider =  _droppingObject.GetComponent<Collider>();
+		_droppingCollider.enabled = false;
 		_carrying =false;
-		_carriedObject.GetComponent<Rigidbody>().isKinematic = false;
+		_dropping = true;
+		_dropOffTimer.Reset();
+		//_carriedObject.GetComponent<Rigidbody>().isKinematic = false;
 		_carriedObject = null;
 	}
 }
