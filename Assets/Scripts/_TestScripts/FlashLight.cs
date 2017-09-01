f1e61c31448304c4fe111ffa396769b4fd2a60c9
YIWEN DAI <daiyw1992@gmail.com>
2017-09-01 15:35:46 -0700

    zoetrope test

---
+++ Assets/Scripts/_TestScripts/FlashLight.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test{
+	public class FlashLight : MonoBehaviour {
+
+		public float totalSeconds;     // The total of seconds the flash wil last
+		public float maxIntensity;     // The maximum intensity the flash will reach
+		public Light myLight;        // Your light
+		Timer _offTimer; 
+		Timer _onTimer;
+		bool _isFlashing = false;
+		bool _isLightOn = false;
+
+
+		void Start(){
+			_offTimer = new Timer (1.0f / 24.0f);
+			_onTimer = new Timer (0.02f);
+		}
+
+		void Update(){
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				_isFlashing = !_isFlashing;
+			}
+
+			if (_isFlashing) {
+				//TurnOnlight ();
+				if (_isLightOn && _onTimer.IsOffCooldown) {
+					TurnOfflight ();
+				} else if (!_isLightOn && _offTimer.IsOffCooldown) {
+					TurnOnlight ();
+				}
+			} else {
+				TurnOfflight();
+			}
+
+
+		}
+
+
+		void TurnOnlight(){
+			myLight.intensity = maxIntensity;
+			_onTimer.Reset ();
+			_isLightOn = true;
+
+		}
+
+		void TurnOfflight(){
+			myLight.intensity = 0;
+			_offTimer.Reset ();
+			_isLightOn = false;
+		}
+	}
+	
+}
+
+
---
+++ Assets/Scripts/_TestScripts/RotateObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test{
+public class RotateObject : MonoBehaviour {
+	[SerializeField] Transform _myTransform;
+	[SerializeField] float _targetSpeed = 100f;
+	bool _isRotating = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			_isRotating = !_isRotating;
+
+		}
+
+		if (_isRotating) {
+			_myTransform.Rotate(0f,0f,-Time.deltaTime*_targetSpeed,Space.World);
+		}
+
+
+		
+	}
+}
+}
+
