03c4c8aede4ab0e301fd41088e5b5d30ccc7ef7b
Jung-Ho Sohn <jsohn96@hotmail.com>
2017-10-03 19:28:18 -0700

    Push camera initi

---
+++ Assets/MusicBoxCameraInitialize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBoxCameraInitialize : MonoBehaviour {
+	[SerializeField] Transform _otherPositionCamera;
+	bool _once = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!_once && Input.GetKeyDown(KeyCode.Space)) {
+			transform.SetPositionAndRotation (_otherPositionCamera.position, _otherPositionCamera.rotation);
+			_once = true;
+		}
+	}
+}
