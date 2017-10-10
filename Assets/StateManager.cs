23e46e483ee3e31b65aa6a7ddb9d884cc900e5cf
Jung-Ho Sohn <junghos@usc.edu>
2017-10-10 13:52:10 -0700

    Push TItle

---
+++ Assets/StateManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour {
+	public static StateManager stateManager;
+
+	// Use this for initialization
+	void Start () {
+		if (stateManager == null) {
+			DontDestroyOnLoad (gameObject);
+			stateManager = this;
+		}
+		else if (stateManager != this) {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
