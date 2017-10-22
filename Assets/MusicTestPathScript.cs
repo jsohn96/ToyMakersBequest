f7394902c39c2660fba6c5d1480bed3e37111c68
Jung-Ho Sohn <jsohn96@hotmail.com>
2017-10-21 21:07:49 -0700

    Music Test

---
+++ Assets/MusicTestPathScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTestPathScript : MonoBehaviour {
+	[SerializeField] MusicTestScript[] _musicTestScripts = new MusicTestScript[14];
+	Timer _moveTimer;
+	int cnt = 0;
+	Vector3 _tempCurrentPos;
+	Vector3 _goalPos;
+	bool _once = false;
+	// Use this for initialization
+	void Start () {
+		_moveTimer = new Timer (1f);
+		_tempCurrentPos = transform.position;
+		_goalPos = _tempCurrentPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_moveTimer.IsOffCooldown) {
+			
+			if (cnt == 0 || _musicTestScripts [cnt - 1]._peg || _musicTestScripts [cnt-1]._removeCircle || (!_musicTestScripts [cnt - 1]._peg && _musicTestScripts [cnt - 1]._isOther)) {
+				if (!_once && cnt>0) {
+					if (_musicTestScripts [cnt-1]._peg) {
+						_musicTestScripts [cnt-1].PlayNote ();
+						_once = true;
+					}
+				}
+				transform.SetParent (null);
+				if (_musicTestScripts [cnt]._removeCircle) {
+					if (!_musicTestScripts [cnt]._isOther) {
+						cnt++;
+						_once = false;
+					}
+				}
+				else if (!_musicTestScripts [cnt]._isOther) {
+					_tempCurrentPos = transform.position;
+					if (_musicTestScripts [cnt]._peg) {
+						_goalPos = _musicTestScripts [cnt].transform.position;
+
+					} else {
+						_goalPos = _musicTestScripts [cnt]._inside.transform.position;
+						transform.SetParent (_musicTestScripts [cnt]._inside.transform);
+					}
+
+					_moveTimer.Reset ();
+
+					if (_musicTestScripts.Length != cnt + 1) {
+						cnt++;
+						_once = false;
+					}
+
+				}
+			}
+		} else {
+			transform.position = Vector3.Lerp (_tempCurrentPos, _goalPos, _moveTimer.PercentTimePassed);
+		}
+	}
+}
---
+++ Assets/MusicTestScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTestScript : MonoBehaviour {
+	AudioSource _audioSource;
+	Quaternion _originRot;
+	Quaternion _otherRot;
+
+	Vector3 _tempAngle;
+	Timer _rotateTimer;
+
+	public bool _isOther = true;
+
+	public bool _peg = false;
+	public Transform _inside;
+	Vector3 _position;
+	Vector3 _originPos;
+
+	public bool _removeCircle = false;
+	// Use this for initialization
+	void Start () {
+		_audioSource = GetComponent<AudioSource> ();
+		_originRot = transform.rotation;
+		_tempAngle = _originRot.eulerAngles;
+		_tempAngle.x += 180.0f;
+		_otherRot = Quaternion.Euler (_tempAngle);
+		if (!_peg) {
+			_inside = transform.GetChild (0);
+		}
+		transform.rotation = _otherRot;
+		if (_removeCircle) {
+			_originPos = transform.position;
+			_position = transform.position;
+			_position.x = 4f;
+
+		}
+	}
+
+
+	public void PlayNote(){
+		_audioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void OnMouseOver() {
+		if (!_peg) {
+			if (Input.GetMouseButtonDown (0)) {
+				if (_isOther) {
+					transform.rotation = _originRot;
+					_isOther = false;
+					if (_removeCircle) {
+						transform.position = _position;
+					}
+				} else {
+					transform.rotation = _otherRot;
+
+						_isOther = true;
+					if (!_removeCircle) {
+						PlayNote ();
+					}
+
+					if (_removeCircle) {
+						transform.position = _originPos;
+					}
+				}
+
+			}
+		}
+	}
+
+	IEnumerator DelayThing(){
+		yield return new WaitForSeconds (1.0f);
+		_isOther = true;
+	}
+}
