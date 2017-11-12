407369b0b122bd3f7d938b007271947c5a8e1996
Jung-Ho Sohn <junghos@usc.edu>
2017-11-11 21:18:31 -0800

    Book in progress to stash others

---
+++ Assets/NotebookPageManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NotebookPage {
+	public int pageNumber;
+	public GameObject[] objectsInPage;
+	public bool nextPageLocked;
+
+	public NotebookPage (
+		int _pageNumber,
+		GameObject[] _objectsInPage,
+		bool _nextPageLocked){
+		pageNumber = _pageNumber;
+		objectsInPage = _objectsInPage;
+		nextPageLocked = _nextPageLocked;
+	}
+}
+
+public class NotebookPageManager : MonoBehaviour {
+
+	int _currentPage = 0;	// Use this for initialization
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
