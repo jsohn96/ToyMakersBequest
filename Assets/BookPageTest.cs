d2db1b47806e8ead2042faec46848ae74a6c916c
YIWEN DAI <daiyw1992@gmail.com>
2017-11-16 00:59:43 -0800

    add book flip temp

---
+++ Assets/BookPageTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookPageTest : MonoBehaviour {
+	Animator _bookAnim;
+
+	// Use this for initialization
+	void Start () {
+		_bookAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			_bookAnim.Play ("Flip");
+		}
+		
+	}
+}
