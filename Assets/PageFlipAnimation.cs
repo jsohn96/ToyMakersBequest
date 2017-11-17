e283ae044a4a74643ff8453af3082a34ed83aa36
Jung-Ho Sohn <junghos@usc.edu>
2017-11-16 20:29:57 -0800

    Book SOmewhat working

---
+++ Assets/PageFlipAnimation.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageFlipAnimation : MonoBehaviour {
+	Animator _bookAnim;
+	bool _isOnRight = true;
+	Timer _bookFlipAnimationTimer;
+	[SerializeField] AnimationClip _bookFlipAnimation;
+
+	IEnumerator _pageZLerpCoroutine;
+	Vector3 _tempPos;
+	float _originY = 0.0f;
+
+	void Start () {
+		_bookAnim = GetComponent<Animator> ();
+		float reducedLength = (_bookFlipAnimation.length * 0.8f);
+		Debug.Log (reducedLength);
+		_bookFlipAnimationTimer = new Timer (reducedLength);
+	
+		Debug.Log (_bookFlipAnimationTimer.IsOffCooldown);
+	}
+
+	public void FlipRight(float newY, bool firstHalf = true) {
+		if (!_isOnRight && _bookFlipAnimationTimer.IsOffCooldown) {
+			_bookAnim.Play ("BackFlip");
+		
+			MoveZ (newY, firstHalf);
+
+			_isOnRight = true;
+		}
+	}
+
+	public void FlipLeft(float newY, bool firstHalf = true) {
+		if (_isOnRight) {
+			if (_bookFlipAnimationTimer.IsOffCooldown) {
+				_bookAnim.Play ("Flip");
+
+				MoveZ (newY, firstHalf);
+
+				_isOnRight = false;
+			}
+		}
+	}
+
+	public void MoveZ(float newY, bool firstHalf = true){
+		_bookFlipAnimationTimer.Reset ();
+		if (_pageZLerpCoroutine != null) {
+			StopCoroutine (_pageZLerpCoroutine);
+		}
+		_pageZLerpCoroutine = LerpZ (newY, firstHalf);
+		StartCoroutine (_pageZLerpCoroutine);
+	}
+
+	public void SwapSides(float startY, float newY) {
+		_tempPos = transform.parent.localPosition;
+		_tempPos.y = startY;
+		transform.parent.localPosition = _tempPos;
+		MoveZ (newY);
+
+		if (_isOnRight) {
+			_bookAnim.Play("Flip", -1, _bookFlipAnimation.length);
+		} else {
+			_bookAnim.Play("BackFlip", -1, _bookFlipAnimation.length);
+		}
+		_isOnRight = !_isOnRight;
+	}
+
+	IEnumerator LerpZ(float newY, bool firstHalf) {
+		_tempPos = transform.parent.localPosition;
+		_originY = _tempPos.y;
+		while (!_bookFlipAnimationTimer.IsOffCooldown) {
+			if (firstHalf) {
+				_tempPos.y = Mathf.Lerp (_originY, newY, (_bookFlipAnimationTimer.PercentTimePassed - 0.5f) * 2f);
+			} else {
+				_tempPos.y = Mathf.Lerp (_originY, newY, (_bookFlipAnimationTimer.PercentTimePassed) * 2f);
+			}
+			transform.parent.localPosition = _tempPos;
+			yield return null;
+		}
+		_tempPos.y = newY;
+		transform.parent.localPosition = _tempPos;
+		yield return null;
+	}
+}
---
+++ Assets/PageFlipManagement.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageFlipManagement : MonoBehaviour {
+	PageFlipAnimation[] _pageFlipAnimations;
+	[SerializeField] Transform[] _pagePool;
+	[SerializeField] Transform _coverPage, _endPage;
+	PageFlipAnimation _coverFlipAnimation, _endFlipAnimation;
+	[SerializeField] Material[] _pageContents;
+
+	float _coverYPos, _endYPos;
+
+	float[] _inBetweenY;
+	// 0: empty slot
+
+	int _currentPage = 0;
+	//0 = CoverPage;
+
+	Vector3 _tempVector3;
+
+	int _totalPages;
+
+	void Start(){
+		_totalPages = _pageContents.Length + 2;
+
+		// gets references for page flipping animation
+		_pageFlipAnimations = new PageFlipAnimation[_pagePool.Length];
+		for (int i = 0; i < _pagePool.Length; i++) {
+			_pageFlipAnimations[i] = _pagePool [i].GetChild (0).GetComponent<PageFlipAnimation> ();
+		}
+		_coverFlipAnimation = _coverPage.GetChild (0).GetComponent<PageFlipAnimation> ();
+		_endFlipAnimation = _endPage.GetChild (0).GetComponent<PageFlipAnimation> ();
+
+		// the z index fighting problem fix init
+		_coverYPos = _coverPage.localPosition.y;
+		_endYPos = _endPage.localPosition.y;
+		float tempDistanceBetweenCovers = _coverYPos - _endYPos;
+		float distanceBetweenEachPage = tempDistanceBetweenCovers / 6f;
+		_inBetweenY = new float[5];
+		for (int i = 0; i < 5; i ++) {
+			_inBetweenY[i] = _coverYPos - ((float)(i+1) * distanceBetweenEachPage);
+			if (i != 4) {
+				_tempVector3 = _pagePool [i].localPosition;
+				_tempVector3.y = _inBetweenY [i];
+				_pagePool [i].localPosition = _tempVector3;
+			}
+		}
+	}
+		
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			if (_currentPage > 2) {
+				if (_currentPage == _totalPages - 1) {
+					//last page
+					_endFlipAnimation.FlipLeft (_coverYPos, false);
+				} else if (_currentPage == _totalPages - 2) {
+					_pageFlipAnimations [MathHelpers.Mod (_currentPage - 1,4)].FlipLeft (_inBetweenY [0], false);
+				} else if (_currentPage == _totalPages - 3) {
+					_pageFlipAnimations [MathHelpers.Mod (_currentPage - 1,4)].FlipLeft (_inBetweenY [1], false);
+				} else {
+					int whichPageToFlip = MathHelpers.Mod((_currentPage -1),4);
+
+					_pageFlipAnimations [whichPageToFlip].FlipLeft (_inBetweenY [2]);
+					_pageFlipAnimations[MathHelpers.Mod((whichPageToFlip + 1),4)].MoveZ(_inBetweenY[2]);
+					_pageFlipAnimations[MathHelpers.Mod((whichPageToFlip - 1),4)].MoveZ(_inBetweenY[3]);
+					_pageFlipAnimations[MathHelpers.Mod((whichPageToFlip - 2),4)].SwapSides(_inBetweenY[4], _inBetweenY[3]);
+				}
+			} else if (_currentPage == 0) {
+				_coverFlipAnimation.FlipLeft (_endYPos);
+			} else if (_currentPage == 1) {
+				_pageFlipAnimations [_currentPage -1].FlipLeft (_inBetweenY[3]);
+			} else if (_currentPage == 2) {
+				_pageFlipAnimations [_currentPage -1].FlipLeft (_inBetweenY[2]);
+			}
+
+			_currentPage = _currentPage + 1;
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			_currentPage = _currentPage - 1;
+
+			if (_currentPage > 2) {
+				if (_currentPage == _totalPages - 1) {
+					//last page
+					_endFlipAnimation.FlipRight (_endYPos);
+				} else if (_currentPage == _totalPages - 2) {
+					_pageFlipAnimations [MathHelpers.Mod (_currentPage - 1,4)].FlipRight (_inBetweenY [3]);
+				} else if (_currentPage == _totalPages - 3) {
+					_pageFlipAnimations [MathHelpers.Mod (_currentPage - 1,4)].FlipRight (_inBetweenY [2]);
+				} else {
+					
+					int whichPageToFlip = MathHelpers.Mod ((_currentPage - 1), 4);
+					_pageFlipAnimations [whichPageToFlip].FlipRight (_inBetweenY [2]);
+					_pageFlipAnimations[MathHelpers.Mod((whichPageToFlip - 1),4)].MoveZ(_inBetweenY[2]);
+					_pageFlipAnimations[MathHelpers.Mod((whichPageToFlip + 1),4)].MoveZ(_inBetweenY[3]);
+					_pageFlipAnimations[MathHelpers.Mod((whichPageToFlip + 2),4)].SwapSides(_inBetweenY[4], _inBetweenY[3]);
+				}
+			} else if (_currentPage == 0) {
+				_coverFlipAnimation.FlipRight (_coverYPos, false);
+			} else if (_currentPage == 1) {
+				_pageFlipAnimations [_currentPage -1].FlipRight (_inBetweenY[0], false);
+			} else if (_currentPage == 2) {
+				_pageFlipAnimations [_currentPage -1].FlipRight (_inBetweenY[1], false);
+			}
+		}
+	}
+
+	// move page on each side a little bit
+	// keep record of up to 5 on each so that only the last 5 will make a difference
+
+	// flip animation
+	// swap parent to the otherside
+
+	// move it a little up && down
+
+	// bring in the last page, put none cover last page in the next thing
+
+
+	//first five are populated
+
+	// last five are also populated
+
+}
