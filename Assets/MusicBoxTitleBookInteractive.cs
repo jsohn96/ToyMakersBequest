ed325b845b0b0521fd182206d2a3f3a910685602
Jung-Ho Sohn <junghos@usc.edu>
2017-11-27 13:25:42 -0800

    Notebook further tests (failed baking)

--- Assets/MusicBoxTitleBookInteractive.cs
+++ Assets/MusicBoxTitleBookInteractive.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class MusicBoxTitleBookInteractive : BookInteractive {
 
 	[SerializeField] Transform _dancer;
-	[SerializeField] Transform _dLight;
+	[SerializeField] Transform _dancerPos;
+	[SerializeField] Transform _spotLightTransform;
 	[SerializeField] Transform _rotateAroundPivot;
+	Light _spotLight;
+	MinMax _spotLightAngle = new MinMax(65f, 80f);
+	MinMax _dancerRotateAngleBound= new MinMax (20f, 120f);
 
-	// Use this for initialization
-	void Start () {
-		
+
+	void OnEnable() {
+		_spotLight = _spotLightTransform.GetComponent<Light> ();
+
+		float tempYRot = _rotateAroundPivot.rotation.eulerAngles.y;
+		tempYRot = tempYRot > 180.0f ? (tempYRot - 360f) * -1f : tempYRot;
+		_spotLight.spotAngle = MathHelpers.LinMap (_dancerRotateAngleBound.Min, _dancerRotateAngleBound.Max, _spotLightAngle.Min, _spotLightAngle.Max, Mathf.Clamp(tempYRot, _dancerRotateAngleBound.Min, _dancerRotateAngleBound.Max));
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		_dancer.Rotate (Vector3.up, 3f);
-		//_rotateAroundPivot.Rotate (Vector3.up, 4f);
-		_dLight.LookAt (_dancer);
+		if (Input.GetKey (KeyCode.A)) {
+			_rotateAroundPivot.Rotate (Vector3.up, 3f);
+			float tempYRot = _rotateAroundPivot.rotation.eulerAngles.y;
+			tempYRot = tempYRot > 180.0f ? (tempYRot - 360f) * -1f : tempYRot;
+			_spotLight.spotAngle = MathHelpers.LinMap (_dancerRotateAngleBound.Min, _dancerRotateAngleBound.Max, _spotLightAngle.Min, _spotLightAngle.Max, Mathf.Clamp(tempYRot, _dancerRotateAngleBound.Min, _dancerRotateAngleBound.Max));
+		}
+		_spotLightTransform.LookAt (_dancerPos);
 	}
 }
