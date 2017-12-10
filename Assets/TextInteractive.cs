c5bb2da798a838690991720effe3e9018ac66ada
Jung-Ho Sohn <junghos@usc.edu>
2017-12-10 13:21:05 -0800

    Click on each sentence to read

--- Assets/TextInteractive.cs
+++ Assets/TextInteractive.cs
@@ -16,19 +16,19 @@ public class TextInteractive : BookInteractive {
 
 	bool _beginVOSequence = false;
 	float _dilateGoal = -0.246f;
-	float _originDialte = 0.0f;
+	float _originDilate = 0.0f;
 
 	IEnumerator _tempCoroutine;
 
 	void Start () {
-		_fadeTimer = new Timer (1f);
+		_fadeTimer = new Timer (0.7f);
 		_glowTimer = new Timer (0.5f);
 
-		for (int i = 0; i < _introVO.clips.Length-1; i++) {
+		for (int i = 0; i <= _introVO.clips.Length-1; i++) {
 			_textMeshPros [i].font = _nonGlowAsset;
 		}
-		_nonGlowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _originDialte);
-		_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _originDialte);
+		_nonGlowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _dilateGoal);
+		_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _dilateGoal);
 	}
 	
 	public override void Interact(){
@@ -39,11 +39,11 @@ public class TextInteractive : BookInteractive {
 	}
 
 	void OnEnable(){
-		for (int i = 0; i < _introVO.clips.Length-1; i++) {
-			_textMeshPros [i].font = _nonGlowAsset;
+		for (int i = 0; i <= _introVO.clips.Length-1; i++) {
+			_textMeshPros [i].font = _glowAsset;
 		}
-		_nonGlowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _originDialte);
-		_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _originDialte);
+		_nonGlowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _dilateGoal);
+		_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _dilateGoal);
 	}
 
 	void OnDisable(){
@@ -62,65 +62,68 @@ public class TextInteractive : BookInteractive {
 		_fadeTimer.Reset ();
 		_textMeshPros [_onWhichVO].font = _glowAsset;
 		float tempDilate;
-		while (!_fadeTimer.IsOffCooldown) {
-			tempDilate = Mathf.Lerp (_originDialte, _dilateGoal, _fadeTimer.PercentTimePassed);
-			_nonGlowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, tempDilate);
-			yield return null;
-		}
-		_nonGlowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _dilateGoal);
-
 		_introVO.audioSource.clip = _introVO.clips [_onWhichVO];
 		//yield return new WaitForSeconds (0.5f);
 		_introVO.audioSource.Play ();
 		_beginVOSequence = true;
+
+		while (!_fadeTimer.IsOffCooldown) {
+			tempDilate = Mathf.Lerp (_dilateGoal, _originDilate, _fadeTimer.PercentTimePassed);
+			_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, tempDilate);
+			yield return null;
+		}
+		_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _originDilate);
+
+
 
 		yield return null;
 	}
 
 	IEnumerator EndVO(){
 		_onWhichVO = 0;
+
+		_glowTimer.Reset ();
+		float tempDilate;
+		while (!_glowTimer.IsOffCooldown) {
+			tempDilate = Mathf.Lerp (_originDilate, _dilateGoal, _glowTimer.PercentTimePassed);
+			_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, tempDilate);
+			yield return null;
+		}
+		_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _dilateGoal);
 		_textMeshPros [_onWhichVO].font = _nonGlowAsset;
-		_glowTimer.Reset ();
-		float tempDilate;
-		while (!_glowTimer.IsOffCooldown) {
-			tempDilate = Mathf.Lerp (_dilateGoal, _originDialte, _glowTimer.PercentTimePassed);
-			_nonGlowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, tempDilate);
-			yield return null;
-		}
-		_nonGlowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _originDialte);
 		yield return null;
 	}
 
-	IEnumerator PlayNext(){
-		_onWhichVO++;
-		_glowTimer.Reset ();
-		float tempDilate;
-		while (!_glowTimer.IsOffCooldown) {
-			tempDilate = Mathf.Lerp (_originDialte, _dilateGoal, _glowTimer.PercentTimePassed);
-			_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, tempDilate);
-			yield return null;
-		}
-		_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _originDialte);
-
-		_textMeshPros [_onWhichVO - 1].font = _nonGlowAsset;
-		_textMeshPros [_onWhichVO].font = _glowAsset;
-		_introVO.audioSource.clip = _introVO.clips [_onWhichVO];
-		_introVO.audioSource.Play ();
-		_beginVOSequence = true;
-		_glowTimer.Reset ();
-		while (!_glowTimer.IsOffCooldown) {
-			tempDilate = Mathf.Lerp (_dilateGoal, _originDialte, _glowTimer.PercentTimePassed);
-			_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, tempDilate);
-			yield return null;
-		}
-		_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _originDialte);
-		yield return null;
-	}
-
+//	IEnumerator PlayNext(){
+//		_onWhichVO++;
+//		_glowTimer.Reset ();
+//		float tempDilate;
+//		while (!_glowTimer.IsOffCooldown) {
+//			tempDilate = Mathf.Lerp (_dilateGoal, _originDilate, _glowTimer.PercentTimePassed);
+//			_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, tempDilate);
+//			yield return null;
+//		}
+//		_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _dilateGoal);
+//
+//		_textMeshPros [_onWhichVO - 1].font = _glowAsset;
+//		_textMeshPros [_onWhichVO].font = _nonGlowAsset;
+//		_introVO.audioSource.clip = _introVO.clips [_onWhichVO];
+//		_introVO.audioSource.Play ();
+//		_beginVOSequence = true;
+//		_glowTimer.Reset ();
+//		while (!_glowTimer.IsOffCooldown) {
+//			tempDilate = Mathf.Lerp (_originDilate, _dilateGoal, _glowTimer.PercentTimePassed);
+//			_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, tempDilate);
+//			yield return null;
+//		}
+//		_glowTextMaterial.SetFloat (ShaderUtilities.ID_FaceDilate, _dilateGoal);
+//		yield return null;
+//	}
+//
 	void FixedUpdate() {
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			Interact ();
-		}
+//		if (Input.GetKeyDown (KeyCode.Space)) {
+//			Interact ();
+//		}
 
 		if (_beginVOSequence) {
 			if (!_introVO.audioSource.isPlaying) {
@@ -129,11 +132,11 @@ public class TextInteractive : BookInteractive {
 					_tempCoroutine = EndVO ();
 					StartCoroutine (_tempCoroutine);
 				}
-				else if (_onWhichVO < _introVO.clips.Length - 1) {
-					_beginVOSequence = false;
-					_tempCoroutine = PlayNext ();
-					StartCoroutine (_tempCoroutine);
-				}
+//				else if (_onWhichVO < _introVO.clips.Length - 1) {
+//					_beginVOSequence = false;
+//					_tempCoroutine = PlayNext ();
+//					StartCoroutine (_tempCoroutine);
+//				}
 			}
 		}
 	}
