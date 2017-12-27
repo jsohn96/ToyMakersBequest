2bc11ab0253844ccd60e50ae501053cdd7e24afc
Jung-Ho Sohn <junghos@usc.edu>
2017-12-26 18:51:18 -0800

    Light controlling began

---
+++ Assets/LightSourceController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSourceController : MonoBehaviour {
+	public LightSystem _thisLightSystem;
+
+	public IEnumerator LightOn(float duration){
+		float timer = 0.0f;
+		float originIntensity = _thisLightSystem.light.intensity;
+		while (timer < duration) {
+			timer += Time.deltaTime;
+			_thisLightSystem.light.intensity = Mathf.Lerp (originIntensity, _thisLightSystem.intensity, timer / duration);
+			yield return null;
+		}
+
+		_thisLightSystem.light.intensity = _thisLightSystem.intensity;
+		yield return null;
+	}
+}
--- Assets/Scripts/MusicBox/MBLightManager.cs
+++ Assets/Scripts/MusicBox/MBLightManager.cs
@@ -2,20 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class LightSystem {
+	public Light light;
+	public float intensity;
+	public Color color;
+	public float range;
+
+	public LightSystem (
+		Light _light,
+		float _intensity,
+		Color _color,
+		float _range){
+		light = _light;
+		intensity = _intensity;
+		color = _color;
+		range = _range;
+	}
+}
+
 public class MBLightManager : MonoBehaviour {
 	[SerializeField] MBLightUtil[] _startLights;
+	[SerializeField] LightSourceController[] _layer1Lights;
+	MusicBoxCameraStates _currentCameraState;
 
 	void OnEnable(){
 		Events.G.AddListener<MBLightManagerEvent> (LightStateHandle);
+		Events.G.AddListener<PathStateManagerEvent> (PathStateManagerHandle);
+		Events.G.AddListener<MBCameraStateManagerEvent> (MBCameraStateHandle);
 	}
 
 	void OnDisable(){
 		Events.G.RemoveListener<MBLightManagerEvent> (LightStateHandle);
+		Events.G.RemoveListener<PathStateManagerEvent> (PathStateManagerHandle);
+		Events.G.RemoveListener<MBCameraStateManagerEvent> (MBCameraStateHandle);
+	}
+
+	void PathStateManagerHandle(PathStateManagerEvent e){
+
+	}
+
+	void MBCameraStateHandle(MBCameraStateManagerEvent e){
+		_currentCameraState = e.activeState;
+		float lerpDuration = e.CamDuration;
+		switch (_currentCameraState) {
+		case MusicBoxCameraStates.intro:
+			break;
+		case MusicBoxCameraStates.activation:
+			StartCoroutine (_layer1Lights [0].LightOn (2.0f));
+			break;
+		default:
+			break;
+		}
 	}
 
 	// Use this for initialization
 	void Start () {
-		
+		_layer1Lights [0]._thisLightSystem.light.intensity = 0.0f;
 	}
 	
 	// Update is called once per frame
