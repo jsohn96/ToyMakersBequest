525456ee26ef09e396672d592987e0057e7f0c2e
YIWEN DAI <daiyw1992@gmail.com>
2018-01-17 00:14:26 -0800

    add lv3

---
+++ Assets/Scripts/MusicBox/MusicBoxManagerTemp.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBoxManagerTemp : MonoBehaviour {
+	
+	[SerializeField] PathNetwork[] _musicPaths;
+
+
+	// Use this for initialization
+	void Awake () {
+
+
+	}
+
+	void Start(){
+		//Events.G.Raise (new DancerChangeMoveEvent (DancerMove.idleDance));
+		_musicPaths [0].SetPathActive (true);
+	}
+
+	void OnEnable(){
+		Events.G.AddListener<PathStateManagerEvent> (PathStateManagerHandle);
+	
+
+	}
+
+	void OnDisable(){
+		Events.G.RemoveListener<PathStateManagerEvent> (PathStateManagerHandle);
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	
+
+	}
+
+	void PathStateManagerHandle(PathStateManagerEvent e){
+		switch (e.activeEvent) {
+		case PathState.none:
+			break;
+	
+		}
+	}
+		
+}
