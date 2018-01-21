0f72fac39c3d0bd04170c70933d62a243453a449
Jung-Ho Sohn <junghos@usc.edu>
2018-01-20 16:28:21 -0800

    Baked Lights test

---
+++ Assets/NonStaticRuntime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonStaticRuntime : MonoBehaviour {
+	void Start () {
+		Transform[] allChildren = GetComponentsInChildren<Transform> ();
+		foreach (Transform child in allChildren) {
+			child.gameObject.isStatic = false;
+		}
+		gameObject.isStatic = false;
+	}
+}
