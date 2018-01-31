f9534dae616aa978850317e726d1d1cab4aca8b7
Jung-Ho Sohn <junghos@usc.edu>
2018-01-31 03:49:44 -0800

    Credits Key Pickup

---
+++ Assets/AlternateDirection/CreditScroll.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class CreditScroll : MonoBehaviour {
+	float speed = 0.02f;
+	[SerializeField] float _delay = 3f;
+	[SerializeField] float _delaySpeedUpDuration = 6f;
+	bool _delayBeforeScroll = false;
+	bool _delaySpeedUp = false;
+	[SerializeField] RectTransform _creditTransform;
+
+	[SerializeField] TextMeshProUGUI _tmp;
+	float textFadeDuration = 0.7f;
+	Color _emptyColor;
+	Color _goalColor;
+
+	void Start(){
+		StartCoroutine (DelayBeforeScroll ());
+		StartCoroutine (DelaySpeedUp ());
+		if (_tmp != null) {
+			_goalColor = _tmp.color;
+			_emptyColor = _goalColor;
+			_emptyColor.a = 0.0f;
+			_tmp.color = _emptyColor;
+		}
+	}
+
+	void Update(){
+		if (Input.GetMouseButton (0)) {
+			if (_delaySpeedUp) {
+				speed = 0.08f;
+			}
+		} else if (Input.GetMouseButtonUp (0)) {
+			speed = 0.02f;
+		}
+	}
+	
+	void FixedUpdate () {
+		if (_delayBeforeScroll) {
+			_creditTransform.Translate (Vector2.up * speed);
+		}
+	}
+
+	IEnumerator DelayBeforeScroll(){
+		yield return new WaitForSeconds (_delay);
+		_delayBeforeScroll = true;
+	}
+
+	IEnumerator DelaySpeedUp(){
+		yield return new WaitForSeconds (_delaySpeedUpDuration);
+		_delaySpeedUp = true;
+		if (_tmp != null) {
+			StartCoroutine (FadeInSpeedUpText ());
+		}
+	}
+
+	IEnumerator FadeInSpeedUpText(){
+		float timer = 0f;
+		while (timer < _delaySpeedUpDuration) {
+			timer += Time.deltaTime;
+			_tmp.color = Color.Lerp (_emptyColor, _goalColor, timer / _delaySpeedUpDuration);
+			yield return null;
+		}
+		_tmp.color = _goalColor;
+		yield return null;
+	}
+}
---
+++ Assets/PickUpKeyCredit.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PickUpKeyCredit : MonoBehaviour {
+	float speed = 0.02f;
+	[SerializeField] float _delay = 3f;
+	[SerializeField] float _delaySpeedUpDuration = 6f;
+	bool _delayBeforeScroll = false;
+	bool _delaySpeedUp = false;
+	[SerializeField] RectTransform _creditTransform;
+	bool _stopForPickUp = false;
+	[SerializeField] Button _button;
+	[SerializeField] Fading _fadeScript;
+
+	[SerializeField] AudioSource _pickUpSound;
+
+	[SerializeField] bool _isForKey = true;
+	bool _readyToEnd = false;
+
+	void Start(){
+		StartCoroutine (DelayBeforeScroll ());
+		StartCoroutine (DelaySpeedUp ());
+	}
+
+	void Update(){
+		if (Input.GetMouseButton (0)) {
+			if (_delaySpeedUp) {
+				speed = 0.08f;
+			}
+		} else if (Input.GetMouseButtonUp (0)) {
+			speed = 0.02f;
+		}
+
+		if (_readyToEnd) {
+			if (Input.GetMouseButtonDown (0)) {
+				StartCoroutine (ChangeLevel ());
+			}
+		}
+	}
+
+	void FixedUpdate () {
+		if (_delayBeforeScroll && !_stopForPickUp) {
+			_creditTransform.Translate (Vector2.up * speed);
+
+			if (_creditTransform.anchoredPosition.y >= 0f) {
+				_stopForPickUp = true;
+				if (_isForKey) {
+					_button.interactable = true;
+				} else {
+					_readyToEnd = true;
+				}
+			}
+		}
+	}
+
+	IEnumerator DelayBeforeScroll(){
+		yield return new WaitForSeconds (_delay);
+		_delayBeforeScroll = true;
+	}
+
+	IEnumerator DelaySpeedUp(){
+		yield return new WaitForSeconds (_delaySpeedUpDuration);
+		_delaySpeedUp = true;
+	}
+
+	public void KeyPickUp(){
+		AltCentralControl._currentState = AltStates.keyUnlock;
+		InventorySystem._instance.AddItem (items.finalKey);
+		GetComponent<Image> ().enabled = false;
+		_pickUpSound.Play ();
+		StartCoroutine (ChangeLevel ());
+	}
+
+
+	IEnumerator ChangeLevel(){
+		yield return new WaitForSeconds(1.5f);
+		float fadeTime = _fadeScript.BeginFade (1);
+		yield return new WaitForSeconds(fadeTime);
+		if (_isForKey) {
+			SceneManager.LoadScene ("ControlRoom");
+		} else {
+			SceneManager.LoadScene (0);
+		}
+	}
+}
