80bbcab9f6bd418ef62102826f8df2b0cbe4ee97
YIWEN DAI <daiyw1992@gmail.com>
2018-05-09 12:17:52 -0700

    add dancer texture

---
+++ Assets/trailerCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trailerCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Animator> ().Play ("pan");
+		}
+		
+	}
+}
